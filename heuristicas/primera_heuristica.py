a0458128031fe32eaa9ffca4447fdd4b66041603
Bony2002 <67442468+Bony2002@users.noreply.github.com>
2024-09-07 01:47:18 -0300

    Update heuristica 1

--- heuristicas/primera_heuristica.py
+++ heuristicas/primera_heuristica.py
@@ -7,9 +7,70 @@ from shapely.geometry import Point, Polygon
 import sys
 import os
 sys.path.append(os.path.abspath("TD8_ProyectoFinal/"))
+from lectura_data.analisis import check_rectangles, calculate_area
 from lectura_data.generacion_mapa import fun_generacion_mapa
 from lectura_data.xml_parsing import xml_data_extractor
 
+
+def solve(polygon, actual_panel, xml_file_path, restrictions, rectangles, panel_size):
+    """Search for different offsets to find the solution that maximizes the number of panels."""
+    solution_num = 0
+    max_panel = 0 
+    best_panels, best_array, best_indentation, best_offset_x, best_offset_y, best_centers = None, None, None, None, None, None
+
+    # Use the original polygon without modification
+    original_polygon = np.array(polygon)
+
+    # Calculate the bounding box of the original polygon
+    min_x = original_polygon[:, 0].min()
+    min_y = original_polygon[:, 1].min()
+    max_x = original_polygon[:, 0].max()      
+    max_y = original_polygon[:, 1].max()
+
+    # Determine grid size based on the bounding box
+    n_x = int(max_x // actual_panel[0] + actual_panel[0])
+    n_y = int(max_y // actual_panel[1] + actual_panel[1])
+
+    # Iterate over possible offsets and indentations
+    for indentation in range(0, int(actual_panel[0])):
+        for offset_x in range(-int(actual_panel[0]), int(actual_panel[0])):
+            for offset_y in range(-int(actual_panel[1]), int(actual_panel[1])):
+                # Generate the array of possible panels
+                Array = generate_panel_arrays(n_x, n_y, actual_panel, indentation, offset_x, offset_y)
+                okay_panels = contains_rectangles(Array, actual_panel, original_polygon)
+                okay_panels, okay_centers = check_panels(okay_panels, actual_panel, panel_size, restrictions,rectangles)
+
+                if len(okay_panels) > max_panel: 
+                    max_panel = len(okay_panels)
+                    best_indentation, best_offset_x, best_offset_y = indentation, offset_x, offset_y
+                    best_centers = okay_centers
+                    best_panels = okay_panels
+                    best_array = Array 
+
+                    filename = "Solution_" + str(solution_num) + "1st"+ ".csv" 
+                    folder_path = os.path.dirname(xml_file_path)
+                    folder_name = os.path.splitext(os.path.basename(xml_file_path))[0]
+                    csv_folder_path = os.path.join(folder_path, folder_name)
+                    os.makedirs(csv_folder_path, exist_ok=True)
+                    csv_filename = os.path.join(csv_folder_path, filename)
+
+                    with open(csv_filename, mode='w', newline='') as file:
+                        writer = csv.writer(file)
+                        
+                        # Optionally write a header
+                        writer.writerow(["Center_X", "Center_Y"])
+                        
+                        # Write each coordinate to the file
+                        writer.writerows(best_centers)
+
+                    solution_num+=1
+                    
+                    print("Maximal number of panels is", max_panel)
+
+                    print("Indentation is {}, offset is ({}, {})".
+                           format(best_indentation, best_offset_x, best_offset_y))
+
+    return  best_centers
 
 def generate_panel_arrays(nx, ny, panel_size, indentation, offset_x, offset_y):
     (dx, dy) = panel_size
@@ -46,67 +107,6 @@ def contains_rectangles(Array, panel_size, polygon):
     okay_panels  = [(x, y) for (x, y) in Array if contains_rectangle(x, y, panel_size, polygon)]
     return okay_panels
 
-
-def solve(polygon, panel_size, xml_file_path, restrictions):
-    """Search for different offsets to find the solution that maximizes the number of panels."""
-    solution_num = 0
-    max_panel = 0 
-    best_panels, best_array, best_indentation, best_offset_x, best_offset_y, best_centers = None, None, None, None, None, None
-
-    # Use the original polygon without modification
-    original_polygon = np.array(polygon)
-
-    # Calculate the bounding box of the original polygon
-    min_x = original_polygon[:, 0].min()
-    min_y = original_polygon[:, 1].min()
-    max_x = original_polygon[:, 0].max()      
-    max_y = original_polygon[:, 1].max()
-
-    # Determine grid size based on the bounding box
-    n_x = int(max_x // panel_size[0] + panel_size[0])
-    n_y = int(max_y // panel_size[1] + panel_size[1])
-
-    # Iterate over possible offsets and indentations
-    for indentation in range(0, int(panel_size[0])):
-        for offset_x in range(-int(panel_size[0]), int(panel_size[0])):
-            for offset_y in range(-int(panel_size[1]), int(panel_size[1])):
-                # Generate the array of possible panels
-                Array = generate_panel_arrays(n_x, n_y, panel_size, indentation, offset_x, offset_y)
-                okay_panels = contains_rectangles(Array, panel_size, original_polygon)
-                okay_panels, okay_centers = check_panels(okay_panels, panel_size, restrictions)
-
-                if len(okay_panels) > max_panel: 
-                    max_panel = len(okay_panels)
-                    best_indentation, best_offset_x, best_offset_y = indentation, offset_x, offset_y
-                    best_centers = okay_centers
-                    best_panels = okay_panels
-                    best_array = Array 
-
-                    filename = "Solution_" + str(solution_num) + "1st"+ ".csv" 
-                    folder_path = os.path.dirname(xml_file_path)
-                    folder_name = os.path.splitext(os.path.basename(xml_file_path))[0]
-                    csv_folder_path = os.path.join(folder_path, folder_name)
-                    os.makedirs(csv_folder_path, exist_ok=True)
-                    csv_filename = os.path.join(csv_folder_path, filename)
-
-                    with open(csv_filename, mode='w', newline='') as file:
-                        writer = csv.writer(file)
-                        
-                        # Optionally write a header
-                        writer.writerow(["Center_X", "Center_Y"])
-                        
-                        # Write each coordinate to the file
-                        writer.writerows(best_centers)
-
-                    solution_num+=1
-                    
-                    print("Maximal number of panels is", max_panel)
-
-                    print("Indentation is {}, offset is ({}, {})".
-                           format(best_indentation, best_offset_x, best_offset_y))
-
-    return best_panels, best_array, best_centers
-
 def check_center(center, restrictions):
     for rest in restrictions:
         center_point = Point(center)
@@ -117,25 +117,37 @@ def check_center(center, restrictions):
     return True
 
 
-def check_panels(panels,panel_size, restrictions):
-    width, heigth = panel_size
+def check_panels(panels, actual_panel, panel_size, restrictions, rectangles):
+    width, heigth = actual_panel
     true_panels = []
     true_centers = []
     for i in range(len(panels)):
         center = (panels[i][0] + width/2, panels[i][1] + heigth/2)
-        if check_center(center,restrictions):
+        if check_center(center,restrictions) and check_rectangles(center,actual_panel,panel_size,rectangles):
             true_panels.append(panels[i])
             true_centers.append(center)
     return true_panels, true_centers
 
 
+def grid_heuristic(polygon, panel_size, xml_file_path, restrictions, rectangles = None):
+    if not rectangles:
+        rectangles = [[] for _ in range(len(panel_size))]
+    for i in range(len(panel_size)):
+        actual_panel = panel_size[i]
+        sub_rectangles = solve(polygon,actual_panel,xml_file_path,restrictions,rectangles,panel_size)
+        rectangles[i].extend(sub_rectangles)
+    return rectangles
+    
+        
+
 
 if __name__ == "__main__":
 
-    # file_path_bony = 'C:/Users/valen/OneDrive/Escritorio/Bony/Di tella/TD8FINAL/TD8_ProyectoFinal/mapas/pol.01.xml'
-    file_path_bony = 'C:/Users/valen/OneDrive/Escritorio/Bony/Di tella/TD8FINAL/TD8_ProyectoFinal/mapas/Entrada_v2.xml'
+    file_path_bony = 'C:/Users/valen/OneDrive/Escritorio/Bony/Di tella/TD8FINAL/TD8_ProyectoFinal/mapas/pol.01.xml'
+    file_path_bony = 'C:/Users/valen/OneDrive/Escritorio/Bony/Di tella/TD8FINAL/TD8_ProyectoFinal/mapas/pol.1s.07.xml'
+    # file_path_bony = 'C:/Users/valen/OneDrive/Escritorio/Bony/Di tella/TD8FINAL/TD8_ProyectoFinal/mapas/Entrada_v2.xml'
     polygon, pads_data, restrictions , angulo = xml_data_extractor(file_path_bony)
-    print(pads_data)
-
-    true_panels, arrays, true_centers = solve(polygon, pads_data, file_path_bony, restrictions)
-    fun_generacion_mapa(polygon,restrictions,true_centers,pads_data)
+    rectangles = grid_heuristic(polygon, pads_data, file_path_bony, restrictions)
+    rectangles = grid_heuristic(polygon, pads_data, file_path_bony, restrictions, rectangles)
+    fun_generacion_mapa(polygon,restrictions,rectangles,pads_data)
+    calculate_area(polygon,rectangles,pads_data)
--- lectura_data/analisis.py
+++ lectura_data/analisis.py
@@ -2,6 +2,8 @@ import matplotlib.patches
 import matplotlib.pyplot
 import numpy as np
 from shapely.geometry import Point, Polygon 
+from triangulator.ear_clipping_method import triangulate
+
 
 def contains_rectangle(x, y, panel_size, polygon):
     (dx, dy) = panel_size
@@ -19,7 +21,6 @@ def check_center(center, restrictions):
     return True
 
 def check_availability(center, center_size, pads_data, polygon, restrictions, rectangles):
-    # Step 1: Check if the rectangle is fully inside the polygon
     dx, dy = center_size
     x, y = center
     x_bl = x - dx / 2
@@ -34,14 +35,12 @@ def check_availability(center, center_size, pads_data, polygon, restrictions, re
     if not np.all(result):
         return False
     
-    # Step 2: Check if the center is within any restriction areas
     for rest in restrictions:
         center_point = Point(center)
         fig_rest = Polygon(rest)
         if center_point.within(fig_rest):
             return False
     
-    # Step 3: Check if the rectangle does not overlap with any other rectangles in the list
     width1, height1 = center_size
     left1 = x - width1 / 2
     right1 = x + width1 / 2
@@ -60,3 +59,48 @@ def check_availability(center, center_size, pads_data, polygon, restrictions, re
                 return False
         
     return True
+
+
+def check_rectangles(center, center_size, pads_data, rectangles):
+    dx, dy = center_size
+    x, y = center
+    width1, height1 = center_size
+    left1 = x - width1 / 2
+    right1 = x + width1 / 2
+    top1 = y - height1 / 2
+    bottom1 = y + height1 / 2
+    for i in range(len(rectangles)): 
+        for center2 in rectangles[i]:
+            width1, height1 = pads_data[i]
+            x2, y2 = center2
+            left2 = x2 - width1 / 2
+            right2 = x2 + width1 / 2
+            top2 = y2 - height1 / 2
+            bottom2 = y2 + height1 / 2
+            
+            if not (left1 >= right2 or left2 >= right1 or top1 >= bottom2 or top2 >= bottom1):
+                return False
+    return True
+
+def calculate_area(polygon, rectangles, panel_size):
+    triangles = triangulate(polygon)
+    areas = []
+    for trian in triangles:
+        t_area = 0.5 * abs(
+            trian[0][0] * (trian[1][1] - trian[2][1]) +
+            trian[1][0] * (trian[2][1] - trian[0][1]) +
+            trian[2][0] * (trian[0][1] - trian[1][1])
+        )
+        areas.append(t_area)
+    cover_area = 0
+    quantity_rect = []
+    total_area = sum(areas)
+    for i in range(len(rectangles)):
+        aux_rects = len(rectangles[i])
+        quantity_rect.append(aux_rects)
+        print(panel_size[i])
+        cover_area += panel_size[i][0]*panel_size[i][1]*aux_rects
+    print(f"Rectangles: {quantity_rect} Area covered: {cover_area/total_area}")     
+
+    
+        
