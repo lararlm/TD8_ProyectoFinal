8c13d5a14654c53e42359a0a4e1076fc6f3a625a
Bony2002 <67442468+Bony2002@users.noreply.github.com>
2024-10-07 18:51:18 -0300

    Nuevos archivos

--- lectura_and_analisis/optimization_functions.py
+++ lectura_and_analisis/optimization_functions.py
@@ -1,5 +1,6 @@
 from lectura_and_analisis.analisis import calculate_area
 import random
+from tqdm import tqdm
 
 import copy
 
@@ -7,21 +8,16 @@ def optimize_area(opt_func,polygon,rectangles,pads_data,restrictions, minimum_re
     optimal_rectangles = copy.deepcopy(rectangles)
     len_opt_rectangles = [len(rect) for rect in optimal_rectangles]
     optimal_area = calculate_area(polygon, len_opt_rectangles, pads_data)
-    for _ in range(iterations):
+    for _ in tqdm(range(iterations)):
         temporal_rectangles = copy.deepcopy(optimal_rectangles)
         for i in range(len(temporal_rectangles)):
             number_of_rectangles = random.randint(minimum_rectangles_to_delete,len(temporal_rectangles[i]))
             rectangles_to_remove = random.sample(temporal_rectangles[i], number_of_rectangles)
-            print("Estos rectangulos se eliminaran:", rectangles_to_remove)
             for rect in rectangles_to_remove:
                 temporal_rectangles[i].remove(rect)
-        print(len_opt_rectangles)
         temporal_rectangles = opt_func(polygon,pads_data,restrictions,temporal_rectangles)
         len_temp_rectangles =  [len(rect) for rect in temporal_rectangles]
-        print(len_temp_rectangles)
         temporal_area = calculate_area(polygon,len_temp_rectangles,pads_data)
-        print(temporal_area)
-
         if temporal_area > optimal_area:
             print("Se encontro un mejor posicionamiento con un area total de :", temporal_area)
             optimal_rectangles = temporal_rectangles.copy()
